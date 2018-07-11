418515433f3463c5887676cf4531102c29a5f86e
isuzuki <ikkuntech@gmail.com>
2018-07-11 15:23:41 +0900

    fixed leaked openrtb list to attribute

--- src/main/scala/io/github/isuzuki/openrtb2_4/request/Native.scala
+++ src/main/scala/io/github/isuzuki/openrtb2_4/request/Native.scala
@@ -1,11 +1,11 @@
 package io.github.isuzuki.openrtb2_4.request
 
-import io.github.isuzuki.openrtb2_4.CreativeAttribute
+import io.github.isuzuki.openrtb2_4.{APIFramework, CreativeAttribute}
 
 trait Native {
   val request: String
   val ver:     Option[String]
-  val api:     List[Int]
+  val api:     List[APIFramework]
   val battr:   List[CreativeAttribute]
   val ext:     Option[Ext]
 }
