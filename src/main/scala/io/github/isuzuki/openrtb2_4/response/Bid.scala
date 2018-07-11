1dfe6a25023c4084c02c27bd1b665d50ae36022a
isuzuki <ikkuntech@gmail.com>
2018-07-11 13:24:29 +0900

    added leaked attributes

--- src/main/scala/io/github/isuzuki/openrtb2_4/response/Bid.scala
+++ src/main/scala/io/github/isuzuki/openrtb2_4/response/Bid.scala
@@ -1,21 +1,25 @@
 package io.github.isuzuki.openrtb2_4.response
 
 trait Bid {
-  val id:      String
-  val impid:   String
-  val price:   Float
-  val adid:    Option[String]
-  val nurl:    Option[String]
-  val adm:     Option[String]
-  val adomain: List[String]
-  val bundle:  Option[String]
-  val iurl:    Option[String]
-  val cid:     Option[String]
-  val crid:    Option[String]
-  val cat:     List[String]
-  val attr:    List[Int]
-  val dealid:  Option[String]
-  val w:       Option[Int]
-  val h:       Option[Int]
-  val ext:     Option[Ext]
+  val id:             String
+  val impid:          String
+  val price:          Float
+  val adid:           Option[String]
+  val nurl:           Option[String]
+  val adm:            Option[String]
+  val adomain:        List[String]
+  val bundle:         Option[String]
+  val iurl:           Option[String]
+  val cid:            Option[String]
+  val crid:           Option[String]
+  val cat:            List[String]
+  val attr:           List[Int]
+  val api:            Option[Int]
+  val protocol:       Option[Int]
+  val qagmediarating: Option[Int]
+  val dealid:         Option[String]
+  val w:              Option[Int]
+  val h:              Option[Int]
+  val exp:            Option[Int]
+  val ext:            Option[Ext]
 }
